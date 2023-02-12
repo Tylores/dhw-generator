18eab04ff5612df84a7eb65a4898855bc0de0f34
Tylores <tylor.slay@gmail.com>
2023-02-12 08:43:51 -0800

    not the perfect way to generate average per second water draws, but it works.

---
+++ plotDraws.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+import datetime
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+
+def loadData(filepath: str) -> pd.DataFrame:
+    data = pd.read_csv(filepath)
+    if data.isnull().values.any():
+        print('data is missing values')
+        exit()
+    return data
+
+if __name__ == '__main__':
+    base_names = ['std-1br-dwh','std-2br-dwh','std-3br-dwh','std-4br-dwh','std-5br-dwh']
+    month = 1
+    for name in base_names:
+        group = loadData(f'outputs/grouped-{name}-{month}.csv')
+        plt.figure()
+        plt.plot(group.draws)
+        plt.savefig(f'outputs/{name}-{month}.png', bbox_inches='tight')
